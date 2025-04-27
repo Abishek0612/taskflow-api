7d13e8e15d3ab45f9d2b91953056156cae53c98e
Abishek0612 <uabishek6@gmail.com>
2025-04-27 18:15:09 +0530

    fix: fix database connection, redis issues and server startup bugs

--- src/queues/scheduled-tasks/scheduled-tasks.module.ts
+++ src/queues/scheduled-tasks/scheduled-tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { BullModule } from '@nestjs/bullmq';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OverdueTasksService } from './overdue-tasks.service';
 import { TasksModule } from '../../modules/tasks/tasks.module';
+import { Task } from '@modules/tasks/entities/task.entity';
 
 @Module({
   imports: [
@@ -11,8 +13,9 @@ import { TasksModule } from '../../modules/tasks/tasks.module';
       name: 'task-processing',
     }),
     TasksModule,
+    TypeOrmModule.forFeature([Task]),
   ],
   providers: [OverdueTasksService],
   exports: [OverdueTasksService],
 })
-export class ScheduledTasksModule {} 
+export class ScheduledTasksModule {}
